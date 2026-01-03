0ecae585dd459dc3bcc1373ad44832172622c1a3
codegaws <codegaws@gmail.com>
2026-01-03 06:46:24 -0500

    actualizado

---
+++ src/main/java/com/george/springsecurity/controllers/WelcomeController.java
@@ -0,0 +1,18 @@
+package com.george.springsecurity.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/welcome")
+public class WelcomeController {
+
+    @GetMapping
+    public Map<String, String> welcome() {
+        return Collections.singletonMap("message", "Welcome to Spring Security!");
+    }
+}
