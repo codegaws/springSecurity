fa260832222162a903214d75671ccc557f4baa7d
codegaws <codegaws@gmail.com>
2026-01-27 15:42:45 -0500

    actualizado

--- src/main/java/com/george/springsecurity/security/MyPasswordEncoder.java
+++ src/main/java/com/george/springsecurity/security/MyPasswordEncoder.java
@@ -6,13 +6,17 @@ import org.springframework.stereotype.Component;
 
 //@Component
 public class MyPasswordEncoder /*implements PasswordEncoder */{
-
+    /*
     public String encode(CharSequence rawPassword) {
         return String.valueOf(rawPassword.toString().hashCode());
     }
+    */
 
+    /*
     public boolean matches(CharSequence rawPassword, String encodedPassword) {
         var passwordAsString= String.valueOf(rawPassword.toString().hashCode());
         return encodedPassword.equals(passwordAsString);
     }
+
+*/
 }
